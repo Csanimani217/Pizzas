1a79d430871d82115eba7a344b8b5d68aefcb058
Steve Conor <csanad12345@live.com>
2019-12-11 13:33:08 +0100

    kons,get,set

--- 2019TobbformosMvcPizzaEgyTabla/2019TobbformosMvcPizzaEgyTabla/model/Futar.cs
+++ 2019TobbformosMvcPizzaEgyTabla/2019TobbformosMvcPizzaEgyTabla/model/Futar.cs
@@ -8,5 +8,48 @@ namespace TobbbformosPizzaAlkalmazasEgyTabla.model
 {
     class Futar
     {
+        private int futarID;
+        private string futarNev;
+        private int futarTelefonszam;
+        //konstruktor
+        public Futar(int id, string name, int telszam)
+        {
+            this.futarID = id;
+            this.futarNev = name;
+            this.futarTelefonszam = telszam;
+        }
+
+        //set
+        public void setID(int id)
+        {
+            this.futarID = id;
+        }
+        public void setName(string name)
+        {
+            this.futarNev = name;
+        }
+        public void setPrice(int price)
+        {
+            this.futarTelefonszam = price;
+        }
+
+        //get
+        public int getId()
+        {
+            return futarID;
+        }
+        public string getNeme()
+        {
+            return futarNev;
+        }
+        public int getPrice()
+        {
+            return futarTelefonszam;
+        }
+
+        //public Futar(int futarID, string futarNev, int futarTelefonszam)
+        {
+
+        }
     }
 }
